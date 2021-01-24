541b3ddf07dfbd21449b6701dbd74c5f60109ff8
tiredWizardSnowball <64823327+tiredWizardSnowball@users.noreply.github.com>
2021-01-23 16:31:34 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var fish = 0
-var gold = 0
+var gold = 100000
 var fishing = 0
 var i = 0
 var fishingText = "Start Fishing"
@@ -8,38 +8,62 @@ var width = 0
 var fishingSpeed = 1
 var rodModifier = 1
 var rod = 0
-var rodCost1 = 50
+var rodCost = [50, 200, 1000, 5000, 20000]
+var addFish = null
 
 function startFishing() {
-    if (i == 0 && fishing == 0) {
-        fishingText = "Stop Fishing"
-        fishing = 1
-        var elem = document.getElementById("myBar")
-        id = setInterval(frame, 10)
+        if (i == 0 && fishing == 0) {
+            fishingText = "Stop Fishing"
+            fishing = 1
+            var elem = document.getElementById("myBar")
+            id = setInterval(frame, 10)
 
-        function frame() {
-            if (width >= 100) {
-                i = 0
-                fish++
-                width = 0
-                elem.style.width = 0
-            } else {
-                width = width + fishingSpeed
-                elem.style.width = width + "%"
+            function frame() {
+                if (rodModifier >= 100) {
+                    elem.style.width = 100 + "%";
+                    fish = fish + (rodModifier * 0.01)
+                }
+                if (width >= 100 && rodModifier <= 100) {
+                    i = 0
+                    fish++
+                    width = 0
+                    elem.style.width = 0
+                } else if (rodModifier <= 100) {
+                    width = width + fishingSpeed
+                    elem.style.width = width + "%"
+                }
+            }
+        } else if (fishing == 1) {
+            fishing = 0
+            clearInterval(id)
+            fishingText = "Start Fishing"
+            if (rodModifier >= 100) {
+                elem.style.width = 0 + "%"
             }
         }
-    } else if (fishing == 1) {
-        fishing = 0
-        clearInterval(id)
-        fishingText = "Start Fishing"
-    }
 }
 
 function shop(itemnumber) {
     if (itemnumber == 1) {
-        if (rod == 0 && gold >= rodCost1) {
-            gold = gold - rodCost1
-            rodModifier = 1.5
+        if (rod == 0 && gold >= rodCost[rod]) {
+            gold = gold - rodCost[rod]
+            rodModifier = 2
+            rod++
+        } else if (rod == 1 && gold >= rodCost[rod]) {
+            gold = gold - rodCost[rod]
+            rodModifier = 5
+            rod++
+        } else if (rod == 2 && gold >= rodCost[rod]) {
+            gold = gold - rodCost[rod]
+            rodModifier = 50
+            rod++
+        } else if (rod == 3 && gold >= rodCost[rod]) {
+            gold = gold - rodCost[rod]
+            rodModifier = 100
+            rod++
+        } else if (rod == 4 && gold >= rodCost[rod]) {
+            gold = gold - rodCost[rod]
+            rodModifier = 1000
             rod++
         }
     }
@@ -84,5 +108,25 @@ function update() {
     element.innerHTML = "You have " + gold + " gold!"
     var element = document.getElementById("sellNumber")
     element.innerHTML = "Sell for " + fish * 2 + " gold!"
+    if (rod == 0) {
+        var element = document.getElementById("rodUpgrade")
+        element.innerHTML = "Beginner Rod (" + rodCost[rod] + " gold)"
+    } else if (rod == 1) {
+        var element = document.getElementById("rodUpgrade")
+        element.innerHTML = "Hobbyist Rod (" + rodCost[rod] + " gold)"
+    } else if (rod == 2) {
+        var element = document.getElementById("rodUpgrade")
+        element.innerHTML = "Fisherman's Rod (" + rodCost[rod] + " gold)"
+    } else if (rod == 3) {
+        var element = document.getElementById("rodUpgrade")
+        element.innerHTML = "Elite Rod (" + rodCost[rod] + " gold)"
+    } else if (rod == 4) {
+        var element = document.getElementById("rodUpgrade")
+        element.innerHTML = "Master Rod (" + rodCost[rod] + " gold)"
+    }
+    else if (rod == 5) {
+        var element = document.getElementById("rodUpgrade")
+        element.innerHTML = "You've bought all the rods!"
+    }
 }
 openTab(event, 'Fish')
